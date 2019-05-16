49d01c68c2942082ed5656df0abac90f84b8c016
ALHELO Marwan <Marwan.ALHELO@cpnv.ch>
2019-05-16 09:32:35 +0200

    update 3

--- Deductions fiscale/Deductions fiscale/Form1.cs
+++ Deductions fiscale/Deductions fiscale/Form1.cs
@@ -20,51 +20,32 @@ namespace Deductions_fiscale
         private void Form1_Load(object sender, EventArgs e)
         {
             Result.Visible = false;
-            textBox3.Enabled = false;
-            textBox4.Enabled = false;
-            textBox5.Enabled = false;
-
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if (checkBox1.Checked==true) {
                 textBox3.Enabled = true;
-            }else
+            }
+            else
             {
                 textBox3.Enabled = false;
             }
         }
 
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
-            int i;
-            if (int.TryParse(textBox4.Text, out i){
-
-            }
-            else {
-
-
-            }
-
-            if (checkBox2.Checked == true) {
-                textBox4.Enabled = true;
-            }
-            else {
-                textBox4.Enabled = false;
-            }
+            
         }
 
-        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            if (checkBox3.Checked == true)
-            {
-                textBox5.Enabled = true;
-            }
-            else
-            {
-                textBox5.Enabled = false;
-            }
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
