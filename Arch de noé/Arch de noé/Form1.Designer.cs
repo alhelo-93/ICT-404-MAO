38a5eddaee6bc5421f74540fd26fbfbf2247a43e
alhelo-93 <47849816+alhelo-93@users.noreply.github.com>
2019-06-17 06:36:21 +0200

    update 13

--- Arch de noé/Arch de noé/Form1.Designer.cs
+++ Arch de noé/Arch de noé/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -47,12 +49,11 @@
             "Kangourou",
             "Perroquet",
             "Rat",
-            "ver de terre"});
+            "Ver de terre"});
             this.listBox1.Location = new System.Drawing.Point(44, 51);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(153, 316);
             this.listBox1.TabIndex = 0;
-            
             // 
             // listBox2
             // 
@@ -61,10 +62,10 @@
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(136, 316);
             this.listBox2.TabIndex = 1;
-            
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(231, 118);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -75,18 +76,42 @@
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(236, 214);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "<===";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Encore à terre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(340, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Abord";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox2);
@@ -94,6 +119,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -102,6 +128,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Arch de noé/Arch de noé/Form1.cs
+++ Arch de noé/Arch de noé/Form1.cs
@@ -10,20 +10,70 @@ using System.Windows.Forms;
 
 namespace Arch_de_noé
 {
+
     public partial class Form1 : Form
     {
+       
+
         public Form1()
         {
             InitializeComponent();
+            { 
+
+
+            }
+           
+
+                
+                
+
+                       
         }
-       
+
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedIndex == -1){
+            
+            
 
-                MessageBox.Show("il faut choisir un élément ","Erreur");
+            if (listBox1.SelectedIndex > -1)
+            {
+                listBox1.Sorted = true;
+                listBox2.Items.Add(listBox1.SelectedItem);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                
+            }
+            else
+            {
+                MessageBox.Show("Il faut choisir un élément ", "Erreur");
+
+            }
+
+
+
+
+
+
+        }
+        private void Button2_Click(object sender, EventArgs e)
+            {
+
+            
+
+            if (listBox2.SelectedIndex >-1)
+            {
+                
+                listBox1.Items.Add(listBox2.SelectedItem);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+               
+            }
+            else
+            {
+               
+                MessageBox.Show("Il faut choisir un élément ", "Erreur");
+                
             }
         }
+        }
     }
-}
+
